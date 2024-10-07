44f929cbebcc12f4d144ca84716b7c38e0ba1da4
felipe_alcantara <fmalcantara@hotmail.com>
2024-10-07 19:23:13 -0300

    Implementando websocket na app

--- app.py
+++ app.py
@@ -3,12 +3,14 @@ from repository.database import db
 from db_models.payment import Payment
 from datetime import datetime, timedelta
 from payments.pix import Pix
+from flask_socketio import SocketIO
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
 app.config["SECRET_KEY"] = "SECRET_KEY_WEBSOCKET"
 
 db.init_app(app)
+socketio = SocketIO(app)
 
 @app.route("/payments/pix", methods=["POST"])
 def create_payment_pix():
@@ -54,6 +56,11 @@ def payment_pix_page(payment_id):
                          qr_code=payment.qr_code
                      
                          )
+#websocket
+@socketio.on('connection')
+def handle_connect():
+  print("Client connection to the server")
+
 
 if __name__ == "__main__":
-   app.run(debug=True)
+   socketio.run(app, debug=True)
